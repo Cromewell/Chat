312e2999fce69a220878a2da264addbbd7ff1438
Cromewell <jrlacherman@googlemail.com>
2016-07-25 22:03:17 +0200

    Fail

--- src/com/cromewell/chat/Main.java
+++ src/com/cromewell/chat/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("view"+File.separator+"ClientView.fxml"));
+        loader.setLocation(Main.class.getResource("view/ClientView.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("Chat");
         primaryStage.setScene(new Scene(root));
